10d5efc783f56a1432ac74ed2140d9437b3434ef
cameronmurdock <murdock.cameron@gmail.com>
2023-09-29 18:26:23 -0600

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,7 +9,6 @@ export default function Web() {
     <div className={styles.container}>
       <h1>Web</h1>
       <Button onClick={() => console.log("Pressed!")} text="Boopies Biatch" />
-      {/* Render the UserForm component */}
       <UserForm />
     </div>
   );
