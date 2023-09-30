c993220055eb43e65e743dd489cb0813fa1385c5
cameronmurdock <murdock.cameron@gmail.com>
2023-09-29 18:23:39 -0600

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "ui";
-import { UserForm } from '../../packages/components/UserForm';
+import { UserForm } from "ui";
 import styles from "../styles/index.module.css";
 
 export default function Web() {
